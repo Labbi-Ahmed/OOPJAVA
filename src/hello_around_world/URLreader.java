b6bc57d7de3c03029d8a9778910527c2eed0a717
Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>
2017-11-24 18:41:01 +0600

    HelloAround World add new Class URLreader in helloaroundWorld package . url completed

--- src/hello_around_world/URLreader.java
+++ src/hello_around_world/URLreader.java
@@ -1,5 +1,18 @@
 package hello_around_world;
 
+import edu.duke.*;
+import java.util.*;
+
+
 public class URLreader {
-    
+    public static void urlread(){
+        URLResource url = new URLResource("https://www.google.com/");
+        for(String ur : url.words()){
+            System.out.println(ur);
+        }
+    }
+
+    public static void main(String[] a){
+        urlread();
+    }
 }
