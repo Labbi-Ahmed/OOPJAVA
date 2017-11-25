92b46103be25dd51e8db0617f4ffbf1928d0b891
Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>
2017-11-26 05:17:18 +0600

    Successfully read all babies name from csv file
    Signed-off-by: Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>

---
+++ src/babys_name/ListOfBabysName.java
@@ -0,0 +1,29 @@
+package babys_name;
+
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
+
+public class ListOfBabysName {
+
+    private void listOfName(CSVParser parser) {
+        for (CSVRecord r : parser){
+            System.out.println("name " + r.get(0) + " Gender " + r.get(1) + " number of births " + r.get(2));
+        }
+
+    }
+
+    public void testing(){
+
+        FileResource fr = new FileResource();
+        CSVParser parser = fr.getCSVParser(false);
+        listOfName(parser);
+    }
+
+
+
+    public static void main(String[] args){
+        ListOfBabysName ob = new ListOfBabysName();
+        ob.testing();
+    }
+}
