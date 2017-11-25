affd1c6132b56bf2fb7a0730ef869f8ebabe143f
Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>
2017-11-25 17:12:42 +0600

    Successfully find out the given URL from Web link
    Signed-off-by: Labbi Ahmed <30775026+labbiahmed@users.noreply.github.com>

--- src/assignment_of_oop_and_design/FindingWebLinkPart_4.java
+++ src/assignment_of_oop_and_design/FindingWebLinkPart_4.java
@@ -1,5 +1,49 @@
 package assignment_of_oop_and_design;
 
+import edu.duke.*;
+
 public class FindingWebLinkPart_4 {
-    
+
+    public String checkURL(String link) {
+
+        int index = link.indexOf("\"");
+       // System.out.println(index);
+        if (index == -1)
+            return "";
+
+        int index2 = link.indexOf("\"", index + 1);
+       // System.out.println(index2);
+        if (index2 == -1)
+            return "";
+        String sub = link.substring(index, index2+1);
+       // System.out.println(sub);
+        if (sub.contentEquals("\"https://www.youtube.com/\""))
+            return sub;
+
+        return "";
+    }
+
+    public  void findURL() {
+
+        URLResource url = new URLResource("https://www.youtube.com/");
+        for (String u : url.words()) {
+            String link = checkURL(u);
+            if(link == "")
+                continue;
+            System.out.println(link);
+
+
+       /* String check = "new url\"youtube.com\"youhfkjdkj";
+        String a= checkURL(check);
+
+        System.out.println(a);*/
+        }
+    }
+
+    public static void main(String[] a){
+
+       FindingWebLinkPart_4 b = new FindingWebLinkPart_4();
+       b.findURL();
+    }
 }
+
